9a1beb3e42aecb69abfcd6fb7f2010e0eaabebf4
Jesse James & Kim Dang <student@epicodus.com>
2015-04-28 11:20:13 -0700

    Add webpage content.

---
+++ app.rb
@@ -0,0 +1,16 @@
+require('sinatra')
+require('sinatra/reloader')
+also_reload('lib/**/*.rb')
+require('./lib/place')
+
+get('/') do
+  @place = Place.all()
+  erb(:index)
+end
+
+post('/place') do
+  place = params.fetch('place')
+  location = Place.new(place)
+  location.save()
+  erb(:success)
+end
--- spec/places_integration_spec.rb
+++ spec/places_integration_spec.rb
@@ -1,4 +1,13 @@
-require('capybara/spec')
+require('capybara/rspec')
 require('./app')
-Capybara.app = Sinatra:Application
+Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
+
+describe('places list path', {:type => :feature}) do
+  it('verifies the user path from the form to success page') do
+    visit('/')
+    fill_in('place', :with => 'Portland, Oregon')
+    click_button('Add Place')
+    expect(page).to have_content('The place has been successfully submitted to your list! Back')
+  end
+end
